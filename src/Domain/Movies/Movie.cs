263e41aa6eda5c1760cf056b97903f067164b918
Vitalik-Litvinchuk <61794076+Vitalik-Litvinchuk@users.noreply.github.com>
2025-01-16 12:49:08 +0200

    feat: update movie model

--- src/Domain/Movies/Movie.cs
+++ src/Domain/Movies/Movie.cs
@@ -7,11 +7,12 @@ using Domain.Sessions;
 
 namespace Domain.Movies;
 
-public class Movie(MovieId movieId, string name, long duration)
+public class Movie(MovieId movieId, string name, long duration, string trailerUrl)
 {
     public MovieId Id { get; } = movieId;
     public string Name { get; private set; } = name;
     public long Duration { get; private set; } = duration;
+    public string TrailerUrl { get; private set; } = trailerUrl;
 
     public ICollection<MovieGenre> Genres { get; } = [];
     public ICollection<MovieTag> Tags { get; } = [];
@@ -20,12 +21,13 @@ public class Movie(MovieId movieId, string name, long duration)
     public ICollection<MovieRating> Ratings { get; } = [];
     public ICollection<PurchaseHistory> PurchaseHistories { get; } = [];
 
-    public void UpdateDatails(string name, long duration)
+    public void UpdateDatails(string name, long duration, string trailerUrl)
     {
         Name = name;
         Duration = duration;
+        TrailerUrl = trailerUrl;
     }
 
-    public static Movie New(MovieId movieId, string name, long duration)
-        => new(movieId, name, duration);
+    public static Movie New(MovieId movieId, string name, long duration, string trailerUrl)
+        => new(movieId, name, duration, trailerUrl);
 }
